534c818da9e54d3f54cc03e6b9ee73d9a22c35ae
faridmia <mdfarid7830@gmail.com>
2024-11-11 08:39:31 +0600

    total sum

---
+++ forloop/part2.php
@@ -0,0 +1,11 @@
+<?php
+
+// Create a  script using a for loop to add all the integers between 0 and 30 and display the sum.
+
+$sum = 0;
+
+for( $i = 1; $i <= 30; $i ++) {
+    $sum += $i;
+}
+
+echo "The sum of the numbers 0 to 30 is: {$sum}";
